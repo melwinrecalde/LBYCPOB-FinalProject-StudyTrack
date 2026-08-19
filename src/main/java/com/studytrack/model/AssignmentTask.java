f4744c97e3e8288eda69f959e90af4793235c90e
Melwin Recalde <melwin_s_recalde@dlsu.edu.ph>
2026-08-20 00:50:42 +0800

    Implement assignment task type

--- src/main/java/com/studytrack/model/AssignmentTask.java
+++ src/main/java/com/studytrack/model/AssignmentTask.java
@@ -1,4 +1,22 @@
 package com.studytrack.model;
 
-public class AssignmentTask {
+import java.time.LocalDate;
+
+public class AssignmentTask extends Task {
+
+    public AssignmentTask(
+            String title,
+            String description,
+            LocalDate dueDate,
+            TaskPriority priority,
+            TaskStatus status,
+            Subject subject
+    ) {
+        super(title, description, dueDate, priority, status, subject);
+    }
+
+    @Override
+    public String getTaskType() {
+        return "Assignment";
+    }
 }
