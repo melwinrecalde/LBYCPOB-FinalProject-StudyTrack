d67ac9ebb914ad20bc29f516ad703c562d02f979
Melwin Recalde <melwin_s_recalde@dlsu.edu.ph>
2026-08-20 00:33:03 +0800

    Add study session screen navigation

--- src/main/java/com/studytrack/controller/DashboardController.java
+++ src/main/java/com/studytrack/controller/DashboardController.java
@@ -1,16 +1,29 @@
 package com.studytrack.controller;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.Node;
+import javafx.event.ActionEvent;
+import javafx.stage.Stage;
 
 public class DashboardController {
 
     @FXML
-    private void handleStartStudying() {
-        Alert alert = new Alert(Alert.AlertType.INFORMATION);
-        alert.setTitle("StudyTrack");
-        alert.setHeaderText("Study Session");
-        alert.setContentText("Your study session is ready to begin!");
-        alert.showAndWait();
+    private void handleStartStudying(ActionEvent event) throws Exception {
+
+        FXMLLoader loader = new FXMLLoader(
+                getClass().getResource("/fxml/study-session.fxml")
+        );
+
+        Scene scene = new Scene(loader.load(), 800, 600);
+
+        Stage stage = (Stage) ((Node) event.getSource())
+                .getScene()
+                .getWindow();
+
+        stage.setScene(scene);
+        stage.setTitle("StudyTrack - Study Session");
+        stage.show();
     }
 }
---
+++ src/main/java/com/studytrack/controller/StudySessionController.java
@@ -0,0 +1,4 @@
+package com.studytrack.controller;
+
+public class StudySessionController {
+}
