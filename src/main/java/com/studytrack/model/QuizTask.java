fe01ab22a9fb9dae200d4e916e55f6c2bb6fe185
Melwin Recalde <melwin_s_recalde@dlsu.edu.ph>
2026-08-20 00:50:55 +0800

    Implement quiz task type

--- src/main/java/com/studytrack/model/QuizTask.java
+++ src/main/java/com/studytrack/model/QuizTask.java
@@ -1,4 +1,22 @@
 package com.studytrack.model;
 
-public class QuizTask {
+import java.time.LocalDate;
+
+public class QuizTask extends Task {
+
+    public QuizTask(
+            String title,
+            String description,
+            LocalDate dueDate,
+            TaskPriority priority,
+            TaskStatus status,
+            Subject subject
+    ) {
+        super(title, description, dueDate, priority, status, subject);
+    }
+
+    @Override
+    public String getTaskType() {
+        return "Quiz";
+    }
 }
