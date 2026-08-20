1d12f5a80437a55871473bacd9b27a17820f6c0d
Melwin Recalde <melwin_s_recalde@dlsu.edu.ph>
2026-08-20 17:00:36 +0800

    Update ProjectTask for due time support

--- src/main/java/com/studytrack/model/ProjectTask.java
+++ src/main/java/com/studytrack/model/ProjectTask.java
@@ -1,6 +1,7 @@
 package com.studytrack.model;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 
 public class ProjectTask extends Task {
 
@@ -8,11 +9,20 @@ public class ProjectTask extends Task {
             String title,
             String description,
             LocalDate dueDate,
+            LocalTime dueTime,
             TaskPriority priority,
             TaskStatus status,
             Subject subject
     ) {
-        super(title, description, dueDate, priority, status, subject);
+        super(
+                title,
+                description,
+                dueDate,
+                dueTime,
+                priority,
+                status,
+                subject
+        );
     }
 
     @Override
