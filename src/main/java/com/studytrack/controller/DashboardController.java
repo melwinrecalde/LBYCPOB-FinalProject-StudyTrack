09ca135c873d1c1ca365502597db2e2b70346666
Melwin Recalde <melwin_s_recalde@dlsu.edu.ph>
2026-08-20 13:08:00 +0800

    Add dashboard task navigation

--- src/main/java/com/studytrack/controller/DashboardController.java
+++ src/main/java/com/studytrack/controller/DashboardController.java
@@ -1,16 +1,36 @@
 package com.studytrack.controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.scene.Node;
-import javafx.event.ActionEvent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class DashboardController {
 
     @FXML
-    private void handleStartStudying(ActionEvent event) throws Exception {
+    private void handleAddTask(ActionEvent event) throws IOException {
+
+        FXMLLoader loader = new FXMLLoader(
+                getClass().getResource("/fxml/add-task.fxml")
+        );
+
+        Scene scene = new Scene(loader.load(), 800, 600);
+
+        Stage stage = (Stage) ((Node) event.getSource())
+                .getScene()
+                .getWindow();
+
+        stage.setScene(scene);
+        stage.setTitle("StudyTrack - Add Task");
+        stage.show();
+    }
+
+    @FXML
+    private void handleStartStudying(ActionEvent event) throws IOException {
 
         FXMLLoader loader = new FXMLLoader(
                 getClass().getResource("/fxml/study-session.fxml")
